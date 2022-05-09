a61237ebcb517160d7696ac7a22c6c717d79fdc8
Leah <amelia_rawls@ccpsnet.net>
2022-05-09 17:17:48 -0400

    server accepts and reads incoming data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//  parse incoming JSON data
+app.use(express.json());
 
 const { notes } = require('./data/db');
 
@@ -8,9 +12,14 @@ app.get('/api/notes', (req, res) => {
    let results = notes;
    if (req.query) {
       results = filterByQuery(req.query, results);
-      
+
    }
    res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+   console.log(req.body);
+   res.json(req.body);
 });
 
 
