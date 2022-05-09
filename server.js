66f085f6f26b2e263743a65ec45450a73b831808
Leah <amelia_rawls@ccpsnet.net>
2022-05-09 18:01:32 -0400

    added validation to data

--- server.js
+++ server.js
@@ -19,10 +19,16 @@ app.post('/api/notes', (req, res) => {
    // set id based on what the next index of the array will be
    req.body.id = notes.length.toString();
 
+   // if any data in req.body is incorrect, send 400 error back
+   if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+   } else {
+   
    // add note to json file and notes array in this function
    const note = createNewNote(req.body, notes);
    
    res.json(note);
+   }
 });
 
 function createNewNote(body, notesArray) {
@@ -36,7 +42,16 @@ function createNewNote(body, notesArray) {
    return note;
 }
 
+function validateNote(note) {
+   if (!note.title || typeof note.title !== 'string') {
+      return false;
+   }
 
+   if (!note.text || typeof note.text !== 'string') {
+      return false;
+   }
+   return true;
+}
 
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}!`);
